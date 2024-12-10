231a7bb3af72b635bc7f912a89bacdd120aa898d
Tawdi <ahmedtawdi00@gmail.com>
2024-12-10 12:25:20 +0100

    modifier les lien de navbar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,14 +1,14 @@
 <header>
-    <nav class="bg-blue-50 flex justify-between py-5 px-8">
+    <nav class="bg-blue-50 flex justify-between items-center py-5 px-8">
         <div>
-            <a href="index.php" >
-                <span class="sr-only">Your Company</span>
-                <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="logo of versionhub">
+            <a href="/" class="font-bold text-[1.5rem]" >
+            version<span class="text-indigo-700">hub</span>
+                <!-- <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="logo of versionhub"> -->
             </a>
         </div>
         <div class="flex gap-12">
-            <a href="/">Home</a>
-            <a href="/packages">Packages</a>
+            <a class="font-bold hover:scale-105 transition-all text-indigo-700  hover:text-indigo-900" href="/">Home</a>
+            <a class="font-bold hover:scale-105 transition-all text-indigo-700  hover:text-indigo-900" href="/packages.php">Packages</a>
         </div>
     </nav>
 </header>
