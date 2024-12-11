0a5b2cc1979bb7947a2589d334439dfc89d5178e
Tawdi <ahmedtawdi00@gmail.com>
2024-12-12 00:17:15 +0100

    ajoute page 404

---
+++ views/404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/output.css">
+    <title>Document</title>
+</head>
+<body class= " flex items-center justify-center bg-blue-200 h-screen w-screen">
+   <div class="w-1/2 text-center text-blue-800 ">
+   <p class="text-2xl mb-3 font-bold  ">:(</p>
+   <p class="text-5xl font-bold  ">404</p>
+   <a href="/" class="hover:underline">home page</a>
+   </div>
+  
+</body>
+</html>
