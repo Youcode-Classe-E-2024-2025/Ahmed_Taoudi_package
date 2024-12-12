41538e3c520eec6f69dd9f36c3d160add4f632ee
Tawdi <ahmedtawdi00@gmail.com>
2024-12-12 11:42:42 +0100

    affiche les author pour un package (inner join)

--- controllers/package.php
+++ controllers/package.php
@@ -5,6 +5,6 @@ $id = $_GET['id'];
 $package =fetch("select * from Package where id =  $id ");
 
 $versions = fetchAll(" select * from Version v where v.package_id = $id");
-
+$authors = fetchAll(" select a.id as id, name ,email , biograph from Author a inner join package_author pa on a.id = pa.author_id where pa.package_id = $id ");
 // dd($versions);
 require('views/package.view.php');
--- views/package.view.php
+++ views/package.view.php
@@ -9,9 +9,9 @@
 </head>
 
 <body class="bg-blue-200">
-    <div class=" ">
+    <div  class="h-screen flex flex-col justify-start">
 
-        <?php require('./views/partials/navbar.php') ?>
+        <?php require('views/partials/navbar.php') ?>
 
         <main class="text-center content-center p-10">
             <h1 class="text-4xl font-bold text-indigo-800 p-10"><?= $package['name']?></h1>
@@ -19,6 +19,22 @@
 
             
             <a class=" py-2 px-5 rounded  hover:bg-white hover:text-indigo-700   text-white bg-indigo-700" href="<?= $package['repository_url']?>">(github) install le package </a>
+            <h2 class="text-3xl font-bold text-indigo-800 p-10">
+                Authors :
+            </h2>
+            <div class="flex flex-col justify-center items-center  gap-4 my-5">
+            <?php foreach($authors as $author):?>
+                <div class= "w-3/4 sm:w-1/3 py-2 px-5 rounded hover:bg-white hover:text-indigo-700   text-white bg-indigo-700">
+                   <?= $author['name']?> 
+                   <button type="button"> modifier</button>
+                   <details >
+                       <p class="my-4">e-mail :  <?= $author['email'] ?> </p>
+                        <p class="my-4"> bio :  <?= $author['biograph'] ?>  </p>
+                    </details>
+                </div>
+                <?php endforeach ;?>
+                </div>
+
             <h2 class="text-3xl font-bold text-indigo-800 p-10">
                 les versions :
             </h2>
@@ -34,7 +50,7 @@
             <?php endforeach ;?>
             </div>
         </main>
-        <footer></footer>
+        <?php require('views/partials/footer.php');?>
     </div>
 </body>
 
