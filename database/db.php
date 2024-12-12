cc05414efe3f8b217890c2827be8d6954c9b701a
Tawdi <ahmedtawdi00@gmail.com>
2024-12-12 22:51:09 +0100

    modifier les fonction de db.php et ajoute de execute_sql func

--- database/db.php
+++ database/db.php
@@ -4,20 +4,25 @@ require('config.php');
 
 $dsn ="mysql:host=$host;port=$port;user=$user;password=$password;dbname=$dbname";
 // dd($dsn);
-
+$pdo = new PDO($dsn);
+// global $dsn;
 function fetchAll($query){
-    global $dsn;
-    $pdo = new PDO($dsn);
+global $pdo;
 $statement = $pdo->prepare($query);
 $statement->execute();
 return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function fetch($query){
-    global $dsn;
-    $pdo = new PDO($dsn);
+    global $pdo;
 $statement = $pdo->prepare($query);
 $statement->execute();
 return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+function execute_sql($query){
+    global $pdo;
+$statement = $pdo->prepare($query);
+$statement->execute();
 }
 
